7a5ecb9f62f1aad58f73e1ad182c93ded50b3cd0
Hanno Rein <hanno@hanno-rein.de>
2013-01-27 09:42:08 -0500

    before automatic update
    automatic update: generateASCIITables

---
+++ update_everything.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+commit (){
+  git add *
+  git commit -a -m "$1"
+}
+
+
+commit "before automatic update"
+
+pushd open_exoplanet_catalogue
+git pull
+popd
+commit "automatic update: pulling new open_exoplanet_catalogue"
+
+python generateASCIITables.python
+commit "automatic update: generateASCIITables"
+
+#git push
