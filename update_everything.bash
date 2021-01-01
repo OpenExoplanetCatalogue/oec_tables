687cc0f236bf277d0c65f150313e9149a4aee574
oec-bot-webhook <webhook@openexoplanetcataloque.com>
2021-01-01 23:14:56 +0100

    deleted submodule (now just a link)

--- update_everything.bash
+++ update_everything.bash
@@ -6,12 +6,9 @@ commit (){
 }
 
 
-commit "before automatic update"
-
 pushd open_exoplanet_catalogue
 git pull
 popd
-commit "automatic update: pulling new open_exoplanet_catalogue"
 
 python generateASCIITables.python
 commit "automatic update: generateASCIITables"
